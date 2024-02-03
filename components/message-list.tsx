1c311d2d5aff52253b486366870597e73cd92e65
dbrackovic2 <dbrackovic2@etf.unsa.ba>
2024-02-03 22:58:51 +0100

    update message-list component

--- components/message-list.tsx
+++ components/message-list.tsx
@@ -1,46 +1,74 @@
 import { useQuery, gql } from "@apollo/client";
-import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
+// import { useEffect } from "react";
+// import { useInView } from "react-intersection-observer";
 
 import type { Message as IMessage } from "@/components/message";
 import { Message } from "@/components/message";
 
+type Props = {
+  messages: string[];
+};
+
 const GetRecentMessagesQuery = gql`
   query GetRecentMessages($last: Int) {
-    messageCollection(last: $last) {
-      edges {
-        node {
-          id
-          username
-          avatar
-          body
-          likes
-          createdAt
+    mongoDB {
+      messageCollection(last: $last) {
+        edges {
+          node {
+            id
+            username
+            receiverUsername
+            avatar
+            body
+          }
         }
       }
     }
   }
 `;
 
-export const MessageList = () => {
-  const [scrollRef, inView, entry] = useInView({
-    trackVisibility: true,
-    delay: 1000,
-  });
+const GetMessagesByIDQuery = gql`
+query GetMessagesByID($last: Int, $ids: [ID]) {
+  mongoDB {
+    messageCollection(last: $last, filter: {
+      id: {
+        in: $ids
+      }
+    }) {
+      edges {
+        node {
+          id
+          username
+          receiverUsername
+          avatar
+          body
+        }
+      }
+    }
+  }
+}
+`;
+
+export const MessageList = ({messages}: Props) => {
+  // const [scrollRef, inView, entry] = useInView({
+  //   trackVisibility: true,
+  //   delay: 1000,
+  // });
 
   const { loading, error, data } = useQuery<{
-    messageCollection: { edges: { node: IMessage }[] };
-  }>(GetRecentMessagesQuery, {
+    mongoDB: {messageCollection: { edges: { node: IMessage }[] }};
+  }>(GetMessagesByIDQuery, {
     variables: {
-      "last": 10,
+      "last": 1000,
+      "ids": messages,
     },
   });
 
-  useEffect(() => {
-    if (entry?.target) {
-      entry.target.scrollIntoView({ behavior: "smooth", block: "end" });
-    }
-  }, [data?.messageCollection.edges.length, entry?.target]);
+  // useEffect(() => {
+  //   if (entry?.target) {
+  //     entry.target.scrollIntoView({ behavior: "smooth", block: "end" });
+  //   }
+  // }, [data?.mongoDB.messageCollection.edges.length, entry?.target]);
 
   if (loading)
     return (
@@ -51,12 +79,12 @@ export const MessageList = () => {
 
   if (error)
     return (
-      <p className="text-white">Something went wrong. {error.toString()} <br></br> <button>Get Again</button></p>
+      <p className="text-white">Something went wrong. {error.toString()}</p>
     );
 
   return (
-    <div className="flex flex-col w-full space-y-3 overflow-y-scroll no-scrollbar">
-      {!inView && data?.messageCollection.edges.length && (
+    <div className="flex flex-col max-h-[70vh] w-full space-y-3 overflow-y-scroll">
+      {/* {!inView && data?.mongoDB.messageCollection.edges.length && (
         <div className="py-1.5 w-full px-3 z-10 text-xs absolute flex justify-center bottom-0 mb-[120px] inset-x-0">
           <button
             className="py-1.5 px-3 text-xs bg-[#1c1c1f] border border-[#363739] rounded-full text-white font-medium"
@@ -67,11 +95,11 @@ export const MessageList = () => {
             Scroll to see latest messages
           </button>
         </div>
-      )}
-      {data?.messageCollection?.edges?.map(({ node }) => (
+      )} */}
+      {data?.mongoDB.messageCollection?.edges?.map(({ node }) => (
         <Message key={node?.id} message={node} />
       ))}
-      <div ref={scrollRef} />
+      {/* <div ref={scrollRef} /> */}
     </div>
   );
 };
