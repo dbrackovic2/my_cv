3c787f4b7ef4b0947b1b16e760576976cce0c6a2
dbrackovic2 <dbrackovic2@etf.unsa.ba>
2024-02-03 22:56:56 +0100

    update contactMe component

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,36 +1,19 @@
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
-import { MessageList } from "@/components/message-list";
-import { NewMessageForm } from "@/components/new-message-form";
-
-// type Inputs = {
-//   name: string;
-//   email: string;
-//   subject: string;
-//   message: string;
-// };
-
-// type Props = {
-//   pageInfo: PageInfo;
-// };
+import { Chats } from "@/components/Chats";
 
 function ContactMe() {
   const { data: session, status } = useSession();
+  const [chatWithDajan, setChatWithDajan] = React.useState(false);
+  const chatWithDajanOnClick = () => setChatWithDajan(!chatWithDajan);
 
   return (
-    <div
-      className="h-screen flex relative flex-col text-center md:text-left
-    md:flex-row max-w-7xl justify-evenly mx-auto items-center"
-    >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 opacity-20 text-2xl">
-        Contact
-      </h3>
-
-      <div className="flex flex-col space-y-3 md:space-y-6 lg:space-y-10">
+    <div className="h-screen text-center md:text-left md:flex-row max-w-7xl justify-evenly mx-auto items-center">
+      <div className="space-y-3 md:space-y-6 lg:space-y-10">
         {session ? (
           <div>
-            <div className="flex space-x-1">
+            <div className="flex space-x-1 pt-14">
               {session?.user?.image && (
                 <div className="w-12 h-12 rounded overflow-hidden">
                   <Image
@@ -48,22 +31,13 @@ function ContactMe() {
               >
                 Sign out
               </button>
+              <button onClick={chatWithDajanOnClick} className="bg-white/5 rounded h-12 px-6 font-medium text-white border border-transparent">Chat with Dajan</button>
             </div>
-            <div className="flex-1 overflow-y-scroll no-scrollbar p-6">
-              <div className="max-w-4xl mx-auto">
-                <div className="flex justify-between items-center">
-                  <MessageList />
-                </div>
-              </div>
-            </div>
-            <div className="p-6 bg-white/5 border-t border-[#363739]">
-              <div className="max-w-4xl mx-auto">
-                <NewMessageForm />
-              </div>
-            </div>
+            {chatWithDajan && <Chats username={session?.username ?? ''}/>}
           </div>
         ) : (
           <div className="h-full flex items-center justify-center flex-col space-y-2.5">
+            <h3 className="pt-10 uppercase tracking-[20px] text-gray-500 opacity-20 text-2xl">Contact</h3>
             {status === "loading" ? null : (
               <div className="flex items-center">
                 <button
